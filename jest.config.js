5190198f33f32ac2f81861a8cbc0347bf3d4604c
BlairJ <blair.jacobs@hypr.co.nz>
2022-01-31 17:25:10 +1300

    Initial setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node', 
+  reporters: [ "default", "jest-junit" ] 
+};
+
+// Set environment variables required for integration tests here
+//process.env.P8_B2CProvisioningApiToken=undefined
+
