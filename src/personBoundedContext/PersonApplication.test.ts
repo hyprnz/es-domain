18c9db584caf4185525624ddef4dc0dccbea0878
g <gareth.evans@hypr.co.nz>
2022-03-08 08:58:44 +1300

    Fixed handler.

--- src/personBoundedContext/PersonApplication.test.ts
+++ src/personBoundedContext/PersonApplication.test.ts
@@ -1,32 +1,32 @@
-import { EntityEvent, Uuid } from "..";
-import { ReadModelMemoryRepository } from "../readModelRepository/ReadModelMemoryRepository";
-import { AggregateRootRepository } from "../writeModelRepository/AggregateRootRepository";
-import { InMemoryEventStoreRepository } from "../writeModelRepository/InMemoryEventStoreRepository";
-import { PersonService } from "./service/personService";
+import {EntityEvent, Uuid} from "..";
+import {ReadModelMemoryRepository} from "../readModelRepository/ReadModelMemoryRepository";
+import {AggregateRootRepository} from "../writeModelRepository/AggregateRootRepository";
+import {InMemoryEventStoreRepository} from "../writeModelRepository/InMemoryEventStoreRepository";
+import {PersonService} from "./service/personService";
 
 describe('personApplication', () => {
-  // Setup Read Side
-  const readModelRepo = new ReadModelMemoryRepository();
-  const eventBus = (changes: Array<EntityEvent>) => {
-    // const projections = []
-    // projections.forEach(x => x(changes, readModelRepo))
-  }
+    // Setup Read Side
+    const readModelRepo = new ReadModelMemoryRepository();
+    const handler = async (changes: Array<EntityEvent>) => {
+        // const projections = []
+        // projections.forEach(x => x(changes, readModelRepo))
+    }
 
-  // Setup Write side
-  const personWriteRepository = new AggregateRootRepository(new InMemoryEventStoreRepository())
-  personWriteRepository.subscribeToChangesSynchronously(eventBus)
-  const personService = new PersonService(personWriteRepository)
+    // Setup Write side
+    const personWriteRepository = new AggregateRootRepository(new InMemoryEventStoreRepository())
+    personWriteRepository.subscribeToChangesSynchronously(handler)
+    const personService = new PersonService(personWriteRepository)
 
-  it('Updates entities and read models', async () => {
-    // Perform actions
-    const person1Id = Uuid.createV4()
-    const dogId = Uuid.createV4()
+    it('Updates entities and read models', async () => {
+        // Perform actions
+        const person1Id = Uuid.createV4()
+        const dogId = Uuid.createV4()
 
-    await personService.onboardPerson(person1Id)
-    await personService.adoptDog(person1Id, dogId, "Rufus")
-    await personService.microchipDog(person1Id, dogId)
+        await personService.onboardPerson(person1Id)
+        await personService.adoptDog(person1Id, dogId, "Rufus")
+        await personService.microchipDog(person1Id, dogId)
 
-    readModelRepo.printAll()
-  })
+        readModelRepo.printAll()
+    })
 
 })
