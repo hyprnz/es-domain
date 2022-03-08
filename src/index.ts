4c67a6741e3396ab282b3d293d7a192e002f969e
g <gareth.evans@hypr.co.nz>
2022-03-08 21:27:22 +1300

    Added missing exports.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 export * as Uuid from './eventSourcing/UUID'
 export {AggregateError} from './eventSourcing/AggregateError'
 export {
-  ChangeEvent,
-  EntityEvent,
-  Message
+    ChangeEvent,
+    EntityEvent,
+    Message
 } from './eventSourcing/MessageTypes'
-
 export {WriteModelRepository} from './writeModelRepository/WriteModelRepository'
-
 export {
-  makeProjection,   
-  Projection,
-  ProjectionRow,
-  ProjectionAction,
-  StaticProjectionEventHandler,
+    makeProjection,
+    Projection,
+    ProjectionRow,
+    ProjectionAction,
+    StaticProjectionEventHandler,
 
 } from './readModelRepository/Projection'
-
-
-
 export {EntityBase} from './eventSourcing/EntityBase'
 export {AbstractChangeEvent} from './eventSourcing/AbstractChangeEvent'
 export {AggregateContainer} from './eventSourcing/AggregateContainer'
-
-export * as example from './deviceBoundedContext';
 export {StaticEventHandler} from "./eventSourcing/Entity";
 export {EntityConstructor} from "./eventSourcing/Entity";
 export {Entity} from "./eventSourcing/Entity";
 export {ParentAggregate} from "./eventSourcing/Aggregate";
 export {Aggregate} from "./eventSourcing/Aggregate";
 export {ReadModelRepository} from "./readModelRepository/ReadModelRepository";
+export {InMemoryEventStoreRepository} from "./writeModelRepository/InMemoryEventStoreRepository";
+export {AggregateRootRepository} from "./writeModelRepository/AggregateRootRepository";
+export {ExternalEventStoreInMemoryRepository} from "./eventStoreExternal/ExternalEventStoreInMemoryRepository";
+export {EventStoreExternal} from "./eventStoreExternal/EventStoreExternal";
+export {ExternalEventBuilder} from "./eventStoreExternal/ExternalEventBuilder";
+
+export * as example from './deviceBoundedContext';
