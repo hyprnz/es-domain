a4a0b15ac6309ab50c99c0ba22c6431e08f13f2c
Benji Stephenson <bjames.stephenson@gmail.com>
2022-04-07 16:56:59 +1200

    Add UUID type assertion

--- src/eventSourcing/UUID.ts
+++ src/eventSourcing/UUID.ts
@@ -6,11 +6,15 @@ const nullUUID = '00000000-0000-0000-0000-000000000000'
 export type UUID = string & { [validUUID]: true }
 
 export function assertValidUUID(input: string): asserts input is UUID {
-  let s = '' + input
-  const match = s.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
-  if (match === null) {
+  if (!isValidUUID(input)) {
     throw new Error(`The string: ${input} is not a valid UUID`)
   }
+}
+
+export function isValidUUID(input: string): input is UUID {
+  const s = '' + input
+  const match = s.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
+  return match === null
 }
 
 export function makeFromString(uuid: string): UUID {
