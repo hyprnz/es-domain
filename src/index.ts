d8e920b39c70399b229b4251026d8bd49b4c055c
BlairJ <blair.jacobs@hypr.co.nz>
2022-09-13 11:09:24 +1200

    Export required types from write model

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 export * as Uuid from './eventSourcing/UUID'
-export * from './eventSourcing/AggregateError'
-export * from './eventSourcing/MessageTypes'
-export * from './writeModelRepository/WriteModelRepository'
-export * from './readModelRepository/Projection'
-export * from './eventSourcing/EntityBase'
+
+export * from './eventSourcing/Aggregate'
 export * from './eventSourcing/AggregateContainer'
+export * from './eventSourcing/AggregateError'
 export * from './eventSourcing/Entity'
-export * from './eventSourcing/Aggregate'
-export * from './eventSourcing/Aggregate'
+export * from './eventSourcing/EntityBase'
+export * from './eventSourcing/MessageTypes'
+
+export * from './readModelRepository/Projection'
 export * from './readModelRepository/ReadModelRepository'
 export * from './readModelRepository/ReadModelMemoryRepository'
-export * from './writeModelRepository/InMemoryEventStore'
-export * from './writeModelRepository/AggregateRepository'
-export * from './writeModelRepository/WriteModelSnapshotRepository'
-export * from './writeModelRepository/SnapshotEventStore'
-export * from './writeModelRepository/WriteModelSnapshotRepository'
-export * from './writeModelRepository/OptimisticConcurrencyError'
+
+export * from './writeModelRepository/index'
+
 
 /** @deprecated */
 export * as external from './eventStoreExternal/index'
---
+++ src/writeModelRepository/index.ts
@@ -0,0 +1,15 @@
+export * from './InternalEventStore'
+export * from './WriteModelRepository'
+
+
+export * from './AggregateRepository'
+export * from './InMemoryEventStore'
+
+// Snapshotting
+export * from './SnapshotEventStore'
+export * from './SnapshotRepository'
+export * from './WriteModelSnapshotRepository'
+
+
+export * from './OptimisticConcurrencyError'
+export * from './WriteModelRepositoryError'
