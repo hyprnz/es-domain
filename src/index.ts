f82ac8b6354f146cd2c1522a752aa42fb67af3dd
BlairJ <blair.jacobs@hypr.co.nz>
2022-09-12 13:18:20 +1200

    deprecate external event store

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,16 @@ export * from './writeModelRepository/InMemoryEventStore'
 export * from './writeModelRepository/AggregateRepository'
 export * from './writeModelRepository/WriteModelSnapshotRepository'
 export * from './writeModelRepository/SnapshotEventStore'
-export * from './eventStoreExternal/ExternalEventStoreInMemoryRepository'
-export * from './eventStoreExternal/EventStoreExternal'
-export * from './eventStoreExternal/ExternalEventBuilder'
-export * as example from './deviceBoundedContext'
 export * from './writeModelRepository/WriteModelSnapshotRepository'
 export * from './writeModelRepository/OptimisticConcurrencyError'
+
+/** @deprecated */
+export * from './eventStoreExternal/ExternalEventStoreInMemoryRepository'
+
+/** @deprecated */
+export * from './eventStoreExternal/EventStoreExternal'
+
+/** @deprecated */
+export * from './eventStoreExternal/ExternalEventBuilder'
+
+export * as example from './deviceBoundedContext'
